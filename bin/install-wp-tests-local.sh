ccdbfb8299df3623296b4f9c8054939e6d80577c
Mikael Lindqvist <li.mikael@gmail.com>
2015-12-22 15:23:28 +0300

    fix tests

--- bin/install-wp-tests-local.sh
+++ bin/install-wp-tests-local.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 export WP_TESTS_DIR=`pwd`/tests/wordpress-tests-lib/
-export WP_CORE_DIR=`pwd`tests/wordpres/
+export WP_CORE_DIR=`pwd`/tests/wordpress/
 ./bin/install-wp-tests.sh wptest root ''
